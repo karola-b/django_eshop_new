bb2b0014bdee7b54e9b003ef5f3f7712c529989a
karola-b <karola.bralewska@gmail.com>
2023-04-18 00:10:55 +0200

    add product create view

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from mainapp import views
 
 urlpatterns = [
-    path('list/', views.ProductListView.as_view(), name='list_view')
+    path('list/', views.ProductListView.as_view(), name='list_view'),
+    path('create/', views.ProductCreateView.as_view(), name='create-view')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 from mainapp.models import Product
 
 
 class ProductListView(ListView):
     model = Product
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = "__all__"
+    success_url = reverse_lazy('list_view')
