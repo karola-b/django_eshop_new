a3d8e27911cfe4d81bdf690f59a4fbbf568e27d7
karola-b <karola.bralewska@gmail.com>
2023-04-17 23:59:00 +0200

    add Product model

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=9, decimal_places=2)
